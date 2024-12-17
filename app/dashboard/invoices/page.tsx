87e870c47cd1d7013743169eddb4766523a627e3
licia03 <lgavina7@gmail.com>
2024-12-17 15:52:21 +0100

    handle errors

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,30 +9,30 @@ import { fetchInvoicesPages } from '@/app/lib/data';
 
 
 export default async function Page({
-    searchParams,
-  }: {
-    searchParams: Promise<{ [key: string]: string | undefined }>
-  }) {
-    const {query: searchParamQuery, page} = await searchParams;
-    const query  = searchParamQuery || '';
-    const currentPage = Number(page) || 1;
-    const totalPages = await fetchInvoicesPages(query)
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+  const { query: searchParamQuery, page } = await searchParams;
+  const query = searchParamQuery || '';
+  const currentPage = Number(page) || 1;
+  const totalPages = await fetchInvoicesPages(query)
 
-    return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
-                <CreateInvoice />
-            </div>
-            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-                <Table query={query} currentPage={currentPage} />
-            </Suspense>
-            <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
